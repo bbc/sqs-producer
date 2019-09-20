bc138680a77bebccec484f2639bb63f679e71ba8
Robert Young <me@iamrobertyoung.co.uk>
2019-09-20 12:13:14 +0100

    Removed console.log

--- test/producer.js
+++ test/producer.js
@@ -65,7 +65,6 @@ describe('Producer', function () {
       sinon.assert.calledOnce(sqs.sendMessageBatch);
       sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
 
-      console.log(result);
       done();
     });
   });
