07446eb70117dc2f6a593af925ebe7a23e7dd9ec
vasyl <vasyl@flash-fx.com.au>
2020-02-14 11:27:10 +1100

    es5 -> es6, node -> 8, callbacks -> promises, jshint -> eslint, drop lodash dep., audit -> 0 vuln.

--- test/producer.js
+++ test/producer.js
@@ -1,32 +1,33 @@
-var Producer = require('../lib/producer');
-var sinon = require('sinon');
-var assert = require('assert');
+const Producer = require('../lib/producer');
+const sinon = require('sinon');
+const assert = require('assert');
 
-var AWS = require('aws-sdk');
-var sqs = new AWS.SQS();
+const AWS = require('aws-sdk');
+let sqs = new AWS.SQS();
 
 describe('Producer', function () {
-  var queueUrl = 'https://dummy-queue';
-  var producer;
+  const queueUrl = 'https://dummy-queue';
+  let producer;
 
   beforeEach(function () {
-    sinon.stub(sqs, 'sendMessageBatch').yields(null, {
-      Failed: []
+    sinon.stub(sqs, 'sendMessageBatch').returns({
+      promise: () => (Promise.resolve({
+        Failed: []
+      }))
     });
 
     producer = new Producer({
-      queueUrl: queueUrl,
-      sqs: sqs
+      queueUrl,
+      sqs
     });
-
   });
 
   afterEach(function () {
     sqs.sendMessageBatch.restore();
   });
 
-  it('sends string messages as a batch', function (done) {
-    var expectedParams = {
+  it('sends string messages as a batch', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'message1',
@@ -40,16 +41,13 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    producer.send(['message1', 'message2'], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send(['message1', 'message2']);
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('accepts a single message instead of an array', function (done) {
-    var expectedParams = {
+  it('accepts a single message instead of an array', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'message1',
@@ -59,16 +57,13 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    producer.send('message1', function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send('message1');
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('sends object messages as a batch', function (done) {
-    var expectedParams = {
+  it('sends object messages as a batch', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'id1',
@@ -82,25 +77,22 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1'
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send([message1, message2], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send([message1, message2]);
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('sends object messages with attributes as a batch', function (done) {
-    var expectedParams = {
+  it('sends object messages with attributes as a batch', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'id1',
@@ -120,7 +112,7 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       messageAttributes: {
@@ -130,21 +122,18 @@ describe('Producer', function () {
         }
       }
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send([message1, message2], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send([message1, message2]);
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('sends object messages with FIFO params as a batch', function (done) {
-    var expectedParams = {
+  it('sends object messages with FIFO params as a batch', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'id1',
@@ -162,29 +151,26 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       delaySeconds: 2,
       groupId: 'group1'
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2',
       delaySeconds: 3,
       groupId: 'group2'
     };
 
-    producer.send([message1, message2], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send([message1, message2]);
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('sends both string and object messages as a batch', function (done) {
-    var expectedParams = {
+  it('sends both string and object messages as a batch', async () => {
+    const expectedParams = {
       Entries: [
         {
           Id: 'message1',
@@ -198,96 +184,100 @@ describe('Producer', function () {
       QueueUrl: queueUrl
     };
 
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['message1', message2], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledOnce(sqs.sendMessageBatch);
-      sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
-      done();
-    });
+    await producer.send(['message1', message2]);
+    sinon.assert.calledOnce(sqs.sendMessageBatch);
+    sinon.assert.calledWith(sqs.sendMessageBatch, expectedParams);
   });
 
-  it('makes multiple batch requests when the number of messages is larger than 10', function (done) {
-    producer.send(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'], function (err) {
-      assert.ifError(err);
-      sinon.assert.calledTwice(sqs.sendMessageBatch);
-      done();
-    });
+  it('makes multiple batch requests when the number of messages is larger than 10', async () => {
+    await producer.send(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']);
+    sinon.assert.calledTwice(sqs.sendMessageBatch);
   });
 
-  it('returns an error when SQS fails', function (done) {
-    var sqsError = new Error('sqs failed');
+  it('returns an error when SQS fails', async () => {
+    const sqsError = new Error('sqs failed');
 
     sqs.sendMessageBatch.restore();
-    sinon.stub(sqs, 'sendMessageBatch').yields(sqsError);
-
-    producer.send(['foo'], function (err) {
-      assert.equal(err, sqsError);
-      done();
+    sinon.stub(sqs, 'sendMessageBatch').returns({
+      promise: () => (Promise.reject(sqsError))
     });
+
+    try {
+      await producer.send(['foo']);
+      assert.fail("should have thrown");
+    } catch (err) {
+      assert.equal(err, sqsError);
+    }
   });
 
-  it('returns an error when messages are neither strings nor objects', function (done) {
-    var errMessage = 'A message can either be an object or a string';
+  it('returns an error when messages are neither strings nor objects', async () => {
+    const errMessage = 'A message can either be an object or a string';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1'
     };
-    var message2 = function () {};
+    const message2 = function () {};
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages have invalid delaySeconds params 1', function (done) {
-    var errMessage = 'Message.delaySeconds value must be a number contained within [0 - 900]';
+  it('returns an error when object messages have invalid delaySeconds params 1', async () => {
+    const errMessage = 'Message.delaySeconds value must be a number contained within [0 - 900]';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       delaySeconds: 'typo'
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages have invalid delaySeconds params 2', function (done) {
-    var errMessage = 'Message.delaySeconds value must be a number contained within [0 - 900]';
+  it('returns an error when object messages have invalid delaySeconds params 2', async () => {
+    const errMessage = 'Message.delaySeconds value must be a number contained within [0 - 900]';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       delaySeconds: 12345678
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages attributes don\'t have a DataType param', function (done) {
-    var errMessage = 'A MessageAttribute must have a DataType key';
+  it('returns an error when object messages attributes don\'t have a DataType param', async () => {
+    const errMessage = 'A MessageAttribute must have a DataType key';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       messageAttributes: {
@@ -296,21 +286,23 @@ describe('Producer', function () {
         }
       }
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages attributes have an invalid DataType param', function (done) {
-    var errMessage = 'The DataType key of a MessageAttribute must be a String';
+  it('returns an error when object messages attributes have an invalid DataType param', async () => {
+    const errMessage = 'The DataType key of a MessageAttribute must be a String';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       messageAttributes: {
@@ -320,159 +312,177 @@ describe('Producer', function () {
         }
       }
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages have invalid id param', function (done) {
-    var errMessage = 'Message.id value must be a string';
+  it('returns an error when object messages have invalid id param', async () => {
+    const errMessage = 'Message.id value must be a string';
 
-    var message1 = {
+    const message1 = {
       id: 1234,
       body: 'body1'
     };
 
-    producer.send(message1, function (err) {
+    try {
+      await producer.send(message1);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
-  
-  it('returns an error when object messages have invalid groupId param', function (done) {
-    var errMessage = 'Message.groupId value must be a string';
 
-    var message1 = {
+  it('returns an error when object messages have invalid groupId param', async () => {
+    const errMessage = 'Message.groupId value must be a string';
+
+    const message1 = {
       id: 'id1',
       body: 'body1',
       groupId: 1234
     };
 
-    producer.send(message1, function (err) {
+    try {
+      await producer.send(message1);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages have invalid deduplicationId param', function (done) {
-    var errMessage = 'Message.deduplicationId value must be a string';
+  it('returns an error when object messages have invalid deduplicationId param', async () => {
+    const errMessage = 'Message.deduplicationId value must be a string';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       body: 'body1',
       groupId: '1234',
       deduplicationId: 1234
     };
 
-    producer.send(message1, function (err) {
+    try {
+      await producer.send(message1);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
-  
-  it('returns an error when fifo messages have no groupId param', function (done) {
-    var errMessage = 'FIFO Queue messages must have \'groupId\' prop';
 
-    var message1 = {
+  it('returns an error when fifo messages have no groupId param', async () => {
+    const errMessage = 'FIFO Queue messages must have \'groupId\' prop';
+
+    const message1 = {
       id: 'id1',
       body: 'body1',
       deduplicationId: '1234'
     };
 
-    producer.send(message1, function (err) {
+    try {
+      await producer.send(message1);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages are not of shape {id, body}', function (done) {
-    var errMessage = 'Object messages must have \'id\' prop';
+  it('returns an error when object messages are not of shape {id, body}', async () => {
+    const errMessage = 'Object messages must have \'id\' prop';
 
-    var message1 = {
+    const message1 = {
       noId: 'noId1',
       body: 'body1'
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error when object messages are not of shape {id, body} 2', function (done) {
-    var errMessage = 'Object messages must have \'body\' prop';
+  it('returns an error when object messages are not of shape {id, body} 2', async () => {
+    const errMessage = 'Object messages must have \'body\' prop';
 
-    var message1 = {
+    const message1 = {
       id: 'id1',
       noBody: 'noBody1'
     };
-    var message2 = {
+    const message2 = {
       id: 'id2',
       body: 'body2'
     };
 
-    producer.send(['foo', message1, message2], function (err) {
+    try {
+      await producer.send(['foo', message1, message2]);
+      assert.fail("should have thrown");
+    } catch (err) {
       assert.equal(err.message, errMessage);
-      done();
-    });
+    }
   });
 
-  it('returns an error identifting the messages that failed', function (done) {
+  it('returns an error identifting the messages that failed', async () => {
+    const errMessage = 'Failed to send messages: message1, message2, message3';
     sqs.sendMessageBatch.restore();
 
-    var failedMessages = [{
+    const failedMessages = [{
       Id: 'message1'
     }, {
       Id: 'message2'
     }, {
       Id: 'message3'
     }];
-    sinon.stub(sqs, 'sendMessageBatch').yields(null, {
-      Failed: failedMessages
+    sinon.stub(sqs, 'sendMessageBatch').returns({
+      promise: () => (Promise.resolve({
+        Failed: failedMessages
+      }))
     });
 
-    producer.send(['message1', 'message2', 'message3'], function (err) {
-      assert.equal(err.message, 'Failed to send messages: message1, message2, message3');
-      done();
-    });
+    try {
+      await producer.send(['message1', 'message2', 'message3']);
+      assert.fail("should have thrown");
+    } catch (err) {
+      assert.equal(err.message, errMessage);
+    }
   });
 
-  it('returns the approximate size of the queue', function (done) {
-    var expected = '10';
+  it('returns the approximate size of the queue', async () => {
+    const expected = '10';
     sinon.stub(sqs, 'getQueueAttributes').withArgs({
       QueueUrl: queueUrl,
       AttributeNames: ['ApproximateNumberOfMessages']
-    }).yields(null, {
-      Attributes: {
-        ApproximateNumberOfMessages: expected
-      }
+    }).returns({
+      promise: () => (Promise.resolve({
+        Attributes: {
+          ApproximateNumberOfMessages: expected
+        }
+      }))
     });
 
-    producer.queueSize(function (err, size) {
-      sqs.getQueueAttributes.restore();
-      assert.ifError(err);
-      assert.strictEqual(size, parseInt(expected));
-      done();
-    });
+    const size = await producer.queueSize(['message1', 'message2', 'message3']);
+    sqs.getQueueAttributes.restore();
+    assert.strictEqual(size, Number(expected));
   });
 
   describe('.create', function () {
-      it('creates a new instance of a Producer', function () {
-        var producer = Producer.create({
-          queueUrl: queueUrl,
-          sqs: sqs
-        });
-        assert(producer instanceof Producer);
+    it('creates a new instance of a Producer', function () {
+      const producer = Producer.create({
+        queueUrl,
+        sqs
       });
+      assert(producer instanceof Producer);
+    });
   });
 });
