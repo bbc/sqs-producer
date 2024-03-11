61246de73911d86118f2c02f483328087eaf3b4f
Kyriakos <627855+knicola@users.noreply.github.com>
2024-03-11 11:48:03 -0400

    attach failed message ids to error object (#100)
    this is to allow programmatic access to the ids,
    rather than having to parse the error message.
    
    Co-authored-by: Kyriakos Nicola <knicola@users.noreply.github.com>
    Co-authored-by: Nicholas Griffin <nicholas.griffin@bbc.co.uk>

---
+++ src/errors.ts
@@ -0,0 +1,11 @@
+export class FailedMessagesError extends Error {
+  /** Ids of messages that failed to send. */
+  public failedMessages: string[];
+  /**
+   * @param failedMessages Ids of messages that failed to send.
+   */
+  constructor(failedMessages: string[]) {
+    super(`Failed to send messages: ${failedMessages.join(', ')}`);
+    this.failedMessages = failedMessages;
+  }
+}
